6ecdaa2914512fc2a33a3807f962a0907d31aebf
Sherry Lam <sherrylam2@gmail.com>
2021-01-28 14:54:12 -0800

    Added Hello World files

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(600, 400);
+  textSize(40);
+  textAlign(CENTER);
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+  fill(255);
+  text("Hello World!!", width/2, height/2)
+} 
